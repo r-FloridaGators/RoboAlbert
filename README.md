f7a9b343bba0c073371d19925ea3de04a0afd3b1
disaster_byte <11808043+disasterbyte@users.noreply.github.com>
2019-09-05 12:22:45 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RoboAlbert
+
+### Installing
+
+```
+git clone https://github.com/r-FloridaGators/RoboAblert && cd RoboAlbert
+pip install -r requirements.txt
+```
+Copy `config_sample.py` to `config.py` and add your reddit bot ID/secret/password
---
+++ bot.py
@@ -0,0 +1,167 @@
+from config import CLIENT_ID, CLIENT_SECRET, PASSWORD
+import datetime
+import schedule
+import requests
+import praw
+import time
+
+
+r = praw.Reddit(client_id=CLIENT_ID,
+                client_secret=CLIENT_SECRET,
+                password=PASSWORD,
+                user_agent='RoboAlbert v0.3',
+                username='RoboAlbert')
+
+
+football_thread = """
+## Florida Gators {home_away} {opponent_school} {opponent_name}
+
+| Team | 1st | 2nd | 3rd | 4th | Total|
+|-----:|:---:|:---:|:---:|:---:|:----:|
+|Florida|{t0_q1}|{t0_q2}|{t0_q3}|{t0_q4}|{t0_total}|
+|{opponent_school}|{t1_q1}|{t1_q2}|{t1_q3}|{t1_q4}|{t1_total}|
+
+---
+
+| TIME | TV | STREAMS | SPREAD | O/U |
+|:----:|:--:|:-------:|:------:|:---:|
+|{start_time}|{broadcast}|VIDEO AUDIO|{spread}|{over_under}|
+"""
+
+
+def check_football():
+    scores = requests.get('http://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard').json()
+    for i in scores['events']:
+        if 'Florida Gators' in i['name']:
+            game_id = i['id']
+            event = requests.get('http://site.api.espn.com/apis/site/v2/sports/football/college-football/summary?event={}'.format(game_id)).json()
+            broadcast = event['header']['competitions'][0]['broadcasts'][0]['media']['shortName']
+            spread = event['pickcenter'][0]['spread']
+            over_under = event['pickcenter'][0]['overUnder']
+            opponent_school = event['boxscore']['teams'][0]['team']['location']
+            opponent_name = event['boxscore']['teams'][0]['team']['name']
+            start_date, start_time = (i.strip() for i in event['header']['competitions'][0]['status']['type']['shortDetail'].split('-'))
+
+            # complete garbage
+            today_date = '/'.join([str(int(i)) for i in datetime.datetime.now().strftime('%m/%d').split('/')])
+
+            thread_title = '[Game Thread] Florida {home_away} {opponent_school} ({start_time}, {broadcast})'.format(
+                home_away = 'vs',
+                opponent_school = opponent_school,
+                start_time = start_time,
+                broadcast = broadcast
+            )
+
+            thread_body = football_thread.format(
+                home_away = 'vs',
+                opponent_school = opponent_school,
+                opponent_name = opponent_name,
+                t0_q1 = 0,
+                t0_q2 = 0,
+                t0_q3 = 0,
+                t0_q4 = 0,
+                t0_total = 0,
+                t1_q1 = 0,
+                t1_q2 = 0,
+                t1_q3 = 0,
+                t1_q4 = 0,
+                t1_total = 0,
+                start_time = start_time,
+                broadcast = broadcast,
+                spread = spread,
+                over_under = over_under
+            )
+
+            hour, minute = start_time.split(':')
+            hour = int(hour)
+            minute = int(minute.split()[0])
+
+            if hour > 10:
+                thread_start = str(hour - 1) + ':' + str(minute).zfill(2)
+            else:
+                thread_start = str(hour - 1 + 12) + ':' + str(minute).zfill(2)
+
+            if today_date == start_date:
+                print('Scheduling game thread for ' + thread_start)
+                schedule.every().day.at(thread_start).do(post_thread, 
+                                                         title=thread_title,
+                                                         body=thread_body,
+                                                         sub='FloridaGatorsDev',
+                                                         game_thread=True)
+
+
+def post_thread(title='', body='', sub='FloridaGators', game_thread=False):
+    try:
+        r.subreddit(sub).submit(title, selftext=body)
+        print('Successfully posted {}'.format(title))
+
+    except Exception as e:
+        print('Failed to post {}: {}'.format(title, e))
+
+    if game_thread:
+        return schedule.CancelJob
+
+
+def thread_monitor():
+    for submission in r.subreddit('FloridaGatorsDev').new(limit=25):
+        post_date = datetime.datetime.fromtimestamp(submission.created_utc)
+        post_age = datetime.datetime.now() - post_date
+
+        # Sticky new game threads
+        if '[Game Thread]' in submission.title:
+            if submission.author == 'RoboAlbert':
+                if post_age.days < 1:
+                    # sort the comments
+                    submission.mod.suggested_sort(sort='new')
+                    if not submission.stickied:
+                        # Sticky the post
+                        submission.mod.sticky(state=True)
+                else:
+                    if submission.stickied:
+                        # remove old sticky
+                        thread = r.submission(id=submission.id)
+                        thread.mod.sticky(state=False)
+
+        # New threads set to contest mode
+        if post_age.total_seconds() < 7200:
+            if post_age.total_seconds() < 3600:
+                submission.mod.contest_mode(state=True)
+            else:
+                submission.mod.contest_mode(state=False)
+
+
+if __name__ == '__main__':
+    # TODO
+    #schedule.every().sunday.at('06:00').do(sunday_qb)
+    #schedule.every().sunday.at('06:00').do(weekly_update)
+
+    # Automoderate
+    schedule.every(30).seconds.do(thread_monitor)
+
+    # Game Threads
+    schedule.every().day.at('06:00').do(check_football)
+
+    # Weekly Threads
+    schedule.every().monday.at('06:00').do(post_thread, title='Monday Moan Thread')
+    schedule.every().wednesday.at('06:00').do(post_thread, title='Whatever Wednesday Thread')
+    schedule.every().wednesday.at('06:00').do(post_thread, title='Weekly Prediction Thread', body='Make your predictions for the games this weekend')
+    schedule.every().thursday.at('06:00').do(post_thread, title='r/FloridaGators Pick\'em Reminder', body='GET YOUR PICKS LOCKED IN')
+    schedule.every().thursday.at('06:00').do(post_thread, title='TRASH TALK THURSDAY', body='SMASH THAT CAPS LOT AND TALK SOME TRASH')
+    schedule.every().friday.at('06:00').do(post_thread, title='Free Talk Friday')
+
+
+    try:
+        print('Starting RoboAlbert...')
+
+        # forever and ever <3
+        while True:
+            schedule.run_pending()
+            time.sleep(5)
+
+    except (KeyboardInterrupt, SystemExit):
+        print('Clearing schedule')
+        schedule.clear()
+        exit()
+    
+    except Exception as e:
+        print(e)
